820eb3bb0c6273e92dcaa7c2e8d8d8fb140723b6
fwq <waitingg1021@163.com>
2018-10-30 22:09:22 +0800

    add narcissisticNumber.js

---
+++ essay/src/narcissisticNumber.js
@@ -0,0 +1,19 @@
+// 找出m~n之间的水仙花数。
+function shuixian(m, n){
+		for (var i = m;i <= n;i++) {
+				let sum = 0;
+				let arr = i.toString().split("");
+				for (var j = 0;j < arr.length;j++) {
+						// console.log(arr[j]);
+						var num = parseFloat(arr[j]);
+						var z = Math.pow(num,3);
+						sum += z;
+				}
+				if (i === sum) {
+						console.log(i);
+				} else {
+						console.log("null");
+				}
+		}
+}
+shuixian(1, 154);
